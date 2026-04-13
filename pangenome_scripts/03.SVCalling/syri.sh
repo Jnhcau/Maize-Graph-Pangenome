afce307f76d8efbc9842cfb4247580e9da36ed9a
Jnhcau <nianhuajia_cau@163.com>
2026-04-13 15:28:49 +0800

    Update syri.sh

--- pangenome_scripts/03.SVCalling/syri.sh
+++ pangenome_scripts/03.SVCalling/syri.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+cat $query.mcoords|sed '1,4d'|cut -f 1-7,12-15 >$query.mcoords.syri
+
 syri --nc 10  --nosnp --prefix $query. -c $query.mcoords.syri -d $query.mdelta -r ref.fa -q $query.ordered.qry.fasta
