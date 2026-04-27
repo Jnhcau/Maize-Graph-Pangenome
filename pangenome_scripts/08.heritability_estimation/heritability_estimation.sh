4a1070472286c3e4e470510cf962624bdefb2082
Jia-nianhua <nianhuajia_cau@163.com>
2026-04-27 11:39:12 +0800

    Update heritability_estimation.sh

--- pangenome_scripts/08.heritability_estimation/heritability_estimation.sh
+++ pangenome_scripts/08.heritability_estimation/heritability_estimation.sh
@@ -1,1 +1,9 @@
+plink --vcf 368sv.fakeALT.vcf.gz --make-bed --out 368sv
+ldak6.2.linux --cut-weights weights --bfile 368sv --max-threads 100
+ldak6.2.linux --calc-weights-all weights   --bfile 368sv --max-threads 100
 
+ldak6.2.linux --calc-kins-direct kinship \
+  --bfile 368sv \
+  --weights weights/weights.all \
+  --power -0.5 \
+  --max-threads 100
