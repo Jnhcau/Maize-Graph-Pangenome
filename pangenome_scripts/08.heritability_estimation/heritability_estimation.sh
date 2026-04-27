31651c99a17941dc9c9e1708681e439554c81c2c
Jia-nianhua <nianhuajia_cau@163.com>
2026-04-27 16:22:16 +0800

    Update heritability_estimation.sh

--- pangenome_scripts/08.heritability_estimation/heritability_estimation.sh
+++ pangenome_scripts/08.heritability_estimation/heritability_estimation.sh
@@ -1,4 +1,4 @@
-plink --vcf 368sv.fakeALT.vcf.gz --make-bed --out 368sv
+plink --vcf All_imputated.vcf --make-bed --out 368sv
 ldak6.2.linux --cut-weights weights --bfile 368sv --max-threads 100
 ldak6.2.linux --calc-weights-all weights   --bfile 368sv --max-threads 100
 
@@ -7,3 +7,8 @@ ldak6.2.linux --calc-kins-direct kinship \
   --weights weights/weights.all \
   --power -0.5 \
   --max-threads 100
+
+plink --allow-extra-chr --threads 20 -bfile 368sv --pca 3  --out 368sv.plink.pca
+
+
+ldak6.2.linux --reml  368sv --pheno pheno_renamed.txt --grm kinship --covar 368sv.plink.pca.eigenvec --constrain YES
